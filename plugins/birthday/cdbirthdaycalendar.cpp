bdaf81e6b5b1315a394870b05065cbd8643257a2
David King <davidk@openismus.com>
2011-09-29 11:13:57 +0200

    Birthday plugin: Do not set an end date for birthday events
    Additionally, make sure that the recurrence of the calendar event is
    updated if the contact is updated.

--- plugins/birthday/cdbirthdaycalendar.cpp
+++ plugins/birthday/cdbirthdaycalendar.cpp
@@ -132,11 +132,7 @@ void CDBirthdayCalendar::updateBirthday(const QContact &contact)
         event->startUpdates();
         event->setUid(calendarEventId(contact.localId()));
         event->setAllDay(true);
-
-        // Recurrence.
-        KCalCore::Recurrence * const rule = event->recurrence();
-        rule->setStartDateTime(event->dtStart());
-        rule->setYearly(1);
+        event->setHasEndDate(false);
 
         // Ensure events appear as birthdays in the calendar, NB#259710.
         event->setCategories(QStringList() << QLatin1String("BIRTHDAY"));
@@ -154,8 +150,13 @@ void CDBirthdayCalendar::updateBirthday(const QContact &contact)
     // Transfer birthday details from contact to calendar event.
     event->setSummary(displayName.label());
 
-    event->setDtStart(KDateTime(contactBirthday.date(), QTime(), KDateTime::ClockTime));
-    event->setDtEnd(KDateTime(contactBirthday.date().addDays(1), QTime(), KDateTime::ClockTime));
+    // Event has only date information, no time.
+    event->setDtStart(KDateTime(contactBirthday.date(), KDateTime::ClockTime));
+
+    // Must always set the recurrence as it depends on the event date.
+    KCalCore::Recurrence * const rule = event->recurrence();
+    rule->setStartDateTime(event->dtStart());
+    rule->setYearly(1);
 
     event->setReadOnly(true);
     event->endUpdates();
