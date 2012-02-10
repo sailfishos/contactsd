086523527beac9bb9a494157f7ec6681e452ed91
Mathias Hasselmann <mathias@openismus.com>
2012-02-10 22:12:53 +0200

    Revert "Fixes: Don't insert nie::contentLastModified into two graphes"
    This reverts commit 162cbcf289a89f69ba1ae6d4a74ba4266f7180c1.

--- plugins/telepathy/cdtpstorage.cpp
+++ plugins/telepathy/cdtpstorage.cpp
@@ -685,8 +685,19 @@ enum UpdateTimestampMode {
 
 static void updateTimestamp(Insert &i, const Value &subject, UpdateTimestampMode mode = UntaggedSignalUpdate)
 {
-    Graph g(mode == TaggedSignalUpdate ? privateGraph : defaultGraph);
-    g.addPattern(subject, nie::contentLastModified::resource(), literalTimeStamp());
+    const Value timestamp = literalTimeStamp();
+
+    Graph g(defaultGraph);
+
+    if (mode == TaggedSignalUpdate) {
+        g = Graph(privateGraph);
+        g.addPattern(subject, nie::contentLastModified::resource(), timestamp);
+        i.addData(g);
+
+        g = Graph(defaultGraph);
+    }
+
+    g.addPattern(subject, nie::contentLastModified::resource(), timestamp);
     i.addData(g);
 }
 
